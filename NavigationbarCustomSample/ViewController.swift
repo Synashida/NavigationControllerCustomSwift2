da8c21ede288d03188f8433908241a1a08b078ce
ashida <ashida@v-force.co.jp>
2016-06-28 22:36:28 +0900

    Initial Commit

---
+++ NavigationbarCustomSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationbarCustomSample
+//
+//  Created by ashida on 2016/06/28.
+//  Copyright © 2016年 v-force.co.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
